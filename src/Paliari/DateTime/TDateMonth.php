3a79f2819a06f55ef5d23fbee6495978dfe98256
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-11-11 09:06:33 -0200

    Adiciona classe para criar data com mes de referencia

---
+++ src/Paliari/DateTime/TDateMonth.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Paliari\DateTime;
+
+/**
+ * Class TDateMonth
+ * @package Paliari\DateTime
+ */
+class TDateMonth extends TDateTime
+{
+    protected function init()
+    {
+        $this->setDay(1)->setHour(0)->setMinute(0)->setSecond(0);
+    }
+
+    /**
+     * Retona data em string para ser impressa.
+     * @return bool|string
+     */
+    public function __toString(){
+        return date('m/Y', $this->getTimestamp());
+    }
+}
